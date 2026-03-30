120c84ad65c008e0a87747b0d29c537c47b7b321
venu <kunreddyvenu88@gmail.com>
2026-03-30 21:29:33 +0530

    my changes

--- backend.sh
+++ backend.sh
@@ -1,37 +1,5 @@
-#!bin/bash
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-echo "please enter DB password"
-read -s mysql_root_password
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-       echo -e "$2 ..$R Failure $N"
-    exit 1 #manually exit code if error comes
-    else
-       echo -e "$2 ..$G Success $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-then
-   echo "pease run the script wit root access."
-   exit 1 #manually exit from command.
-else
-   echo "You are a super user."
-fi
-
-=======
 source ./commonvalues.sh  #we need to mention source and ./other script file
 Check_Root
->>>>>>> c03c9a8 (test)
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabling default nodejs"
 
--- mysql.sh
+++ mysql.sh
@@ -1,36 +1,3 @@
-#!/bin/bash
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-echo "please enter DB password"
-read -s mysql_root_password
-
-VALIDATE(){
-
-    if [ $1 -ne 0 ]
-    then
-       echo -e "$2 ... $R FAILURE $N"
-       exit 1 #manually exit if error comes
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-if [ $USERID -ne 0 ]
-then
-   echo "please run the script with super user."
-   exit 1 #manually exits if error comes.
-else 
-   echo "you are super user."
-
-fi
-
-=======
 source ./commonvalues.sh  #we need to mention source and ./other script file
 Check_Root  #this is function calling from other script that source file
 echo "please enter DB password"
